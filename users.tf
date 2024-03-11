bdb2c9bf887bbf7ae1f6bd1212a72c6fa95ece31
Marius Antonsen <antonsen.marius@gmail.com>
2024-03-11 18:45:18 +0100

    fixed formating issues with users.tf
    Signed-off-by: Marius Antonsen <antonsen.marius@gmail.com>

--- users.tf
+++ users.tf
@@ -1,31 +1,31 @@
 locals {
   admins = toset([
-    "AndersLevin-GroupIT",  # Anders Levin - Fra group, må være der pga lisens
-    "molldk",               # Klaus Møllnitz - Fra group, må være der pga lisens
-    "Tjacsen",              # Tore 
-    "antnsn",               # Marius
-    "MrNewgarden",          # Kim  
-    "Flowlance",            # Daniel  
-    "FredrikMelby",         # Fredrik 
-    "Danielbj01",           # Daniel 
-    "GlennIsb",             # Glenn 
-    "johnrosbe",            # John Roy 
-    "maos999"               # Marius 
+    "AndersLevin-GroupIT", # Anders - Fra group
+    "molldk",              # Klaus  - Fra group
+    "Tjacsen",             # Tore 
+    "antnsn",              # Marius
+    "MrNewgarden",         # Kim  
+    "Flowlance",           # Daniel  
+    "FredrikMelby",        # Fredrik 
+    "Danielbj01",          # Daniel 
+    "GlennIsb",            # Glenn 
+    "johnrosbe",           # John Roy 
+    "maos999"              # Marius 
   ])
 
 
   users = toset([
-    "CMCS-PerH",            # Per Helge
-    "emilihoo",             # Emil 
-    "fabskab",              # Fabrizio
-    "Hawolex",              # Steffen 
-    "irean",                # Sandra 
-    "jenaslak",             # Jens 
-    "KA-T1",                # Karl 
-    "kojarB",               # Kojar  
-    "trymhaak",             # Trym 
-    "vagnes",               # Maxine
-    "xQseb"                 # Sebastian
+    "CMCS-PerH", # Per Helge
+    "emilihoo",  # Emil 
+    "fabskab",   # Fabrizio
+    "Hawolex",   # Steffen 
+    "irean",     # Sandra 
+    "jenaslak",  # Jens 
+    "KA-T1",     # Karl 
+    "kojarB",    # Kojar  
+    "trymhaak",  # Trym 
+    "vagnes",    # Maxine
+    "xQseb"      # Sebastian
   ])
 
 }
